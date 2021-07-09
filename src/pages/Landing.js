513252e508af5ba2d0f46c1dd4f8efe51a3f6ef1
Abelard Bautista <abelardbautista@gmail.com>
2021-07-08 22:39:47 -0700

    Created landing page files.

---
+++ src/pages/Landing.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import { Container, Card, InputGroup, Form, Button } from "react-bootstrap";
+import "./Landing.css";
+
+const Landing = (props) => {
+  return <Container className="LandingWrapper"></Container>;
+};
+
+export default Landing;
