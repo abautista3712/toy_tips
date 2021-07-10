c17c70124b846ad9a31317450cddbb293851e234
Abelard Bautista <abelardbautista@gmail.com>
2021-07-10 00:22:49 -0700

    Added boilerplate sticky footer.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <Router>
       <Navigation />
+      <div>Body</div>
+      <Footer />
     </Router>
   );
 }
