8817347c711af7f6d3ad20be6bf1ea7b8fcd0126
Abelard Bautista <abelardbautista@gmail.com>
2021-07-12 21:24:35 -0700

    Fixed spacing on elements in sticky footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import { Container, Row } from "react-bootstrap";
+// import { Container, Row, Col } from "react-bootstrap";
 
 const Footer = () => {
   const handleClick_Facebook = () => {
@@ -19,31 +19,37 @@ const Footer = () => {
   };
   return (
     <div className="footer">
-      <img
-        src="./assets/images/social_icons/Facebook/color/f_logo_RGB-Blue_1024.png"
-        alt="Facebook Logo"
-        className="social_icons"
-        onClick={handleClick_Facebook}
-      />
-
-      <img
-        src="./assets/images/social_icons/Instagram/insta_logo_gradient.png"
-        alt="Instagram Logo"
-        className="social_icons"
-        onClick={handleClick_Instagram}
-      />
-      <img
-        src="./assets/images/social_icons/Twitter/logo_fill/circle/twtr_logo_circle_blue.png"
-        alt="Twitter Logo"
-        className="social_icons"
-        onClick={handleClick_Twitter}
-      />
-      <img
-        src="./assets/images/social_icons/YouTube/full_color/yt_icon_rgb.png"
-        alt="YouTube Logo"
-        className="social_icons"
-        onClick={handleClick_YouTube}
-      />
+      <div style={{ width: "100%" }}>
+        <img
+          src="./assets/images/social_icons/Facebook/color/f_logo_RGB-Blue_1024.png"
+          alt="Facebook Logo"
+          className="social_icons"
+          onClick={handleClick_Facebook}
+        />
+        <img
+          src="./assets/images/social_icons/Instagram/insta_logo_gradient.png"
+          alt="Instagram Logo"
+          className="social_icons"
+          onClick={handleClick_Instagram}
+        />
+        <img
+          src="./assets/images/social_icons/Twitter/logo_fill/circle/twtr_logo_circle_blue.png"
+          alt="Twitter Logo"
+          className="social_icons"
+          onClick={handleClick_Twitter}
+        />
+        <img
+          src="./assets/images/social_icons/YouTube/full_color/yt_icon_rgb.png"
+          alt="YouTube Logo"
+          className="social_icons"
+          onClick={handleClick_YouTube}
+        />
+      </div>
+      <div
+        style={{ display: "flex", width: "100%", justifyContent: "flex-end" }}
+      >
+        <div>© 2021 Toy Tips Inc.</div>
+      </div>
     </div>
   );
 };
