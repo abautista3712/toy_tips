dbdbb9db4230088df4c1e4ff6186f2880286ae86
Abelard Bautista <abelardbautista@gmail.com>
2021-07-12 22:05:10 -0700

    Added Carosel boilerplate.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import Footer from "./components/Footer";
+import Landing from "./pages/Landing";
 
 function App() {
   return (
     <Router>
       <Navigation />
       <div>Body</div>
+      <Landing />
       <Footer />
     </Router>
   );
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,13 +1,48 @@
 import React, { useState } from "react";
-import { Container, Card, InputGroup, Form, Button } from "react-bootstrap";
-import "./Landing.css";
+import { Carousel } from "react-bootstrap";
+// import "./Landing.css";
 
 const Landing = (props) => {
   return (
-    <Container className="LandingWrapper">
-      <div>Nav Bar Goes Here</div>
-      <div>Carosel Goes Here</div>
-    </Container>
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=First slide&bg=373940"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=Second slide&bg=282c34"
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=Third slide&bg=20232a"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
   );
 };
 
