11c3289cd10d1b3134625e91af001747e403c491
Abelard Bautista <abelardbautista@gmail.com>
2021-07-15 17:44:10 -0700

    Fixed second slide caption.

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -4,29 +4,31 @@ import { Carousel } from "react-bootstrap";
 const Landing = (props) => {
   return (
     <Carousel fade>
-      <Carousel.Item>
+      {/* <Carousel.Item>
         <img
           className="d-block w-100"
           src="./assets/images/original/stock_toys1.jpeg"
           alt="First slide"
         />
-        <Carousel.Caption className="carosel_customSlide1">
+        <Carousel.Caption className="caption_Slide1">
           <h1>Thousand of Vintage Toy Reviews</h1>
           <p>Browse our entire collection of toys from your childhood!</p>
         </Carousel.Caption>
-      </Carousel.Item>
-      {/* <Carousel.Item>
+      </Carousel.Item> */}
+      <Carousel.Item>
         <img
           className="d-block w-100"
           src="./assets/images/original/stock_toys2.jpeg"
           alt="Second slide"
         />
-        <Carousel.Caption>
-          h23>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <Carousel.Caption className="caption_Slide1">
+          <h1>30 Years of Independent Research on Smart Consumer Choices</h1>
+          <p style={{ textAlign: "right", paddingRight: "5%" }}>
+            Learn more about Toy Tips
+          </p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item>
+      {/* <Carousel.Item>
         <img
           className="d-block w-100"
           src="./assets/images/original/stock_toys3.png"
