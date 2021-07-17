f34b24ca657ce863abb1f9c429d7ac6791557371
Abelard Bautista <abelardbautista@gmail.com>
2021-07-16 19:22:32 -0700

    Added routing infrastructure.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import Footer from "./components/Footer";
 import Landing from "./pages/Landing";
@@ -9,7 +9,14 @@ function App() {
     <Router>
       <Navigation />
       {/* Component below will change per page */}
-      <Landing />
+      <Switch>
+        <Route exact path="/">
+          <Landing />
+        </Route>
+        <Route path="/about">
+          <p>TEST</p>
+        </Route>
+      </Switch>
       {/* --- */}
       <Footer />
     </Router>
