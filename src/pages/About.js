2b413257ca7c26eb2971aad02ab8e7ce27e4f494
Abelard Bautista <abelardbautista@gmail.com>
2021-07-18 18:04:50 -0700

    Outlined About page with divs.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const About = (props) => {
-  return <div></div>;
+  return (
+    <div>
+      <div>About Toy Tips</div>
+      <div>About Our Research</div>
+      <div>About Toy Tips Testers</div>
+      <div>What do with tested toys?</div>
+      <div>About Marianne</div>
+    </div>
+  );
 };
 
 export default About;
