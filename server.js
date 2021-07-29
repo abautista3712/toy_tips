cf60a6c249920ca083b85ccd349c08a6515e5400
Abelard Bautista <abelardbautista@gmail.com>
2021-07-28 22:31:24 -0700

    Setup Express web server.

--- server.js
+++ server.js
@@ -2,3 +2,32 @@
 
 // Configure CORS
 // Initialzie & run Express REST APIs
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081",
+};
+
+app.use(cors(corsOptions));
+
+// Parse requests of content-type: application/json
+app.use(bodyParser.json());
+
+// Parse requests of content-type: application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Simple Route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome!" });
+});
+
+// Set PORT, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}.`);
+});
