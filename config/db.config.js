2bab5534c4584d210ff9d847d05ac478bd1f8c49
Abelard Bautista <abelardbautista@gmail.com>
2021-07-28 22:31:53 -0700

    Configured MySQL database & Sequelize.

--- config/db.config.js
+++ config/db.config.js
@@ -1,1 +1,14 @@
 // Exports configuring parameters for MySQL connection & Sequelize
+module.exports = {
+  HOST: "localhost",
+  USER: "root",
+  PASSWORD: "123456",
+  DB: "testdb",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+};
