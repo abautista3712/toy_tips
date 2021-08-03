80e4054a8c2cfa828d5d4c5b75eef14a002afa1b
Abelard Bautista <abelardbautista@gmail.com>
2021-08-02 22:13:18 -0700

    Added comment model boilerplate.

---
+++ models/comment.model.js
@@ -0,0 +1,14 @@
+const { sequelize } = require(".");
+
+module.exports = (sequelize, DataTypes) => {
+  const Comment = sequelize.define("comment", {
+    name: {
+      type: DataTypes.STRING,
+    },
+    text: {
+      type: DataTypes.STRING,
+    },
+  });
+
+  return Comment;
+};
