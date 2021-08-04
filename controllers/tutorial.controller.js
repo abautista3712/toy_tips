90d12517188c27b2535e20fe455bd525265b97f2
Abelard Bautista <abelardbautista@gmail.com>
2021-08-03 20:01:25 -0700

    Added controller to Create and Save new Tutorials.

--- controllers/tutorial.controller.js
+++ controllers/tutorial.controller.js
@@ -1,40 +1,41 @@
 // Controller, like the name implies
 
-const db = require("../models")
-const 
-const Op = db.Sequelize.Op
+const db = require("../models");
+const Tutorial = db.tutorials;
+const Comment = db.comments;
+// const Op = db.Sequelize.Op;
 
-// Create and Save
-exports.create = (req, res) => {
+// // Create and Save
+// exports.create = (req, res) => {};
 
-}
+// // Retrieve all data from the database
+// exports.findAll = (req, res) => {};
 
-// Retrieve all data from the database
-exports.findAll = (req, res) => {
+// // Find single data with an id
+// exports.findOne = (req, res) => {};
 
-}
+// // Update data by id in the request
+// exports.update = (req, res) => {};
 
-// Find single data with an id
-exports.findOne = (req, res) => {
+// // Delete data with the specified id in the request
+// exports.delete = (req, res) => {};
 
-}
+// // Delete all data from the database
+// exports.deleteAll = (req, res) => {};
 
-// Update data by id in the request
-exports.update = (req, res) => {
+// // Find all published data
+// exports.findAllPublished = (req, res) => {};
 
-}
-
-// Delete data with the specified id in the request
-exports.delete = (req, res) => {
-
-}
-
-// Delete all data from the database
-exports.deleteAll = (req, res) => {
-
-}
-
-// Find all published data
-exports.findAllPublished = (req, res) => {
-    
-}
+exports.createTutorial = (tutorial) => {
+  return Tutorial.create({
+    title: tutorial.title,
+    description: tutorial.description,
+  })
+    .then((tutorial) => {
+      console.log(">> Created tutorial: " + JSON.stringify(tutorial, null, 4));
+      return tutorial;
+    })
+    .catch((err) => {
+      console.log(">> Error while creating tutorial: ", err);
+    });
+};
