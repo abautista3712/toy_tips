4278c54b2a0df0b333f2dd15e779ca3755940006
Abelard Bautista <abelardbautista@gmail.com>
2021-08-04 18:13:10 -0700

    Added controller to Create and Save new Comments.

--- controllers/tutorial.controller.js
+++ controllers/tutorial.controller.js
@@ -39,3 +39,18 @@ exports.createTutorial = (tutorial) => {
       console.log(">> Error while creating tutorial: ", err);
     });
 };
+
+exports.createComment = (tutorialId, comment) => {
+  return Comment.create({
+    name: comment.name,
+    text: comment.text,
+    tutorialId: tutorialId,
+  })
+    .then((comment) => {
+      console.log(">> Created comment: " + JSON.stringify(comment, null, 4));
+      return comment;
+    })
+    .catch((err) => {
+      console.log(">> Error while creating comment: ", err);
+    });
+};
