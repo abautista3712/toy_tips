d23be0bcd21dabf4a36d254fae5a981badc818b3
Abelard Bautista <abelardbautista@gmail.com>
2021-09-23 21:47:55 -0700

    Removed Overall Grade. Fixed spacing.

--- src/components/ToySearchCard.js
+++ src/components/ToySearchCard.js
@@ -9,10 +9,10 @@ const ToySearchCard = (props) => {
       <Container>
         {/* Toy Search Card */}
         <Row className="m-2 p-2 card_Row">
-          <Col xs={6} sm={4} md={4} lg={4} xl={4} className="flexCenter_center">
+          <Col xs={3} sm={3} md={3} lg={3} xl={3} className="flexCenter_center">
             <Image src="./assets/icons/default_icons/toys.png" fluid />
           </Col>
-          <Col xs={8} sm={8} md={8} lg={8} xl={8}>
+          <Col xs={6} sm={6} md={6} lg={6} xl={6}>
             <Row className="flexCenter">
               <Col xs={8} sm={8} md={8} lg={8} xl={8}>
                 <Col className="p-0">
@@ -20,39 +20,15 @@ const ToySearchCard = (props) => {
                 </Col>
                 <Col className="p-0">Company Name</Col>
               </Col>
-              <Col
-                xs={3}
-                sm={3}
-                md={3}
-                lg={3}
-                xl={3}
-                className="flexCenter_flexEnd"
-              >
-                Overall Rating:
-              </Col>
-              <Col
-                xs={1}
-                sm={1}
-                md={1}
-                lg={1}
-                xl={1}
-                className="flexCenter_center"
-                style={{ background: "grey" }}
-              >
-                Grade
-              </Col>
             </Row>
             <Row>
-              <Col xs={9} sm={9} md={9} lg={9} xl={9}>
+              <Col>
                 <LoremIpsum p={1} />
               </Col>
-              <Col className="flexCenter_center">
-                <Image
-                  src="./assets/icons/scoring_icons/report_card.png"
-                  fluid
-                />
-              </Col>
             </Row>
+          </Col>
+          <Col xs={3} sm={3} md={3} lg={3} xl={3} className="flexCenter_center">
+            <Image src="./assets/icons/scoring_icons/report_card.png" fluid />
           </Col>
         </Row>
       </Container>
