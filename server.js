1241ad63374a0e7f7804b3dcd0005fdc0b2cdd2b
Abelard Bautista <abelardbautista@gmail.com>
2021-10-05 00:36:56 -0700

    Return db toyname to browser.

--- server.js
+++ server.js
@@ -1,38 +1,67 @@
 // Express web server
-
-// Configure CORS
-// Initialzie & run Express REST APIs
+// -Configure CORS
+// -Initialzie & run Express REST APIs
 
 const express = require("express");
+const app = express();
 const cors = require("cors");
 const path = require("path");
+const mysql = require("mysql");
 
-const app = express();
-
+// Shown if no server connection
 app.use(express.static("app/views/"));
 
-var corsOptions = {
+const corsOptions = {
   origin: "http://localhost:8081",
 };
-
 app.use(cors(corsOptions));
-
-// parse requests of content-type - application/json
+// Parse requests of content-type: application/json
 app.use(express.json()); //updated version of body-parser
-
-// parse requests of content-type - application/x-www-form-urlencoded
+// Parse requests of content-type: application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true })); //updated version of body-parser
 
-const db = require("./app/models");
+// If using Sequelize ORM
+// const db = require("./app/models");
 
 // ---USE AFTER DEVELOPMENT---
-db.sequelize.sync();
+// db.sequelize.sync();
 // ---
 // ---USE DURING DEVELOPMENT---
-// db.sequelize.sync({ force: true }).then(() => {
-//   console.log("Drop and re-sync db.");
-//   //run();
+// db.sequelize
+//   .sync({ force: true })
+//   .then(() => {
+//     console.log("Table and model synced successfully!");
+//     console.log("Drop and re-sync db.");
+//     //run();
+//   })
+//   .catch(() => {
+//     console.log("Error syncing the table and model!");
+//   });
+
+// connection.query("SELECT * from reviews", function (err, rows, fields) {
+//   if (err) console.log(err);
+//   console.log("The solution is: ", rows);
+//   connection.end();
 // });
+
+// Route w MySQL
+const db = mysql.createPool({
+  host: "localhost",
+  // port: 3306,
+  user: "root",
+  password: "",
+  database: "toy_tips_db",
+});
+
+app.get("/api/get", (req, res) => {
+  const sqlSearch = "SELECT ToyName FROM reviews LIMIT 0, 5";
+
+  db.query(sqlSearch, (err, result) => {
+    res.send(result);
+  });
+
+  // res.send("hello World");
+});
 
 // // simple route
 app.get("/*", (req, res) => {
@@ -45,7 +74,9 @@ require("./app/routes/review.routes")(app);
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+  console.log(
+    `Server is running on PORT ${PORT}.\n\rView in browser on "localhost:${PORT}"`
+  );
 });
 
 // abelard's code ----------------------
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ToySearchCard from "../components/ToySearchCard";
+import Axios from "axios";
 
 const Search = (props) => {
+  const [count, setCount] = useState(0);
+  const [toyName, setToyName] = useState("");
+  const [toyNameList, setToyNameList] = useState([]);
+
+  useEffect(() => {
+    Axios.get("http://localhost:8080/api/get").then((response) => {
+      setToyNameList(response.data);
+      console.log(response.data);
+    });
+  }, []);
+
   return (
     <div>
+      {toyNameList.map((val) => {
+        return <h1>Toy Name: {val.ToyName} </h1>;
+      })}
+      {/* <ToySearchCard /> */}
+      {/* <ToySearchCard />
       <ToySearchCard />
       <ToySearchCard />
-      <ToySearchCard />
-      <ToySearchCard />
-      <ToySearchCard />
+      <ToySearchCard /> */}
     </div>
   );
 };
