50c1e1e9bd654e0ea21d527a0e011dd7a8797d08
Abelard Bautista <abelardbautista@gmail.com>
2021-11-01 17:43:05 -0700

    Cleaned unused code. Added documentation.

--- server.js
+++ server.js
@@ -23,12 +23,13 @@ app.use(express.json()); //updated version of body-parser
 // Parse requests of content-type: application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true })); //updated version of body-parser
 
-// Route w MySQL
-// console.log(process.env);
+// Config Params for MySQL
+// via Heroku
 if (process.env.JAWSDB_URL) {
   console.log("Connected to JAWSDB_URL");
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
+  // via Local
   console.log("Connected to local");
   connection = mysql.createPool({
     host: "localhost",
@@ -39,34 +40,27 @@ if (process.env.JAWSDB_URL) {
   });
 }
 
+// GET Route for MySQL
 app.get("/api/get", (req, res) => {
   const sqlSearch =
     "SELECT ToyName, AgeRange1, categories, company_name, Character_Development, Kids_Rating, Long_Review, Motor_Movement, product_website, reviewer_longreview, Social_Interaction, ToyTipsRating, Thinking_Skills FROM reviews WHERE Long_Review IS NOT NULL AND Long_Review != ''";
 
+  // Connect to MySQL. Send result to GET route at /api/get
   connection.query(sqlSearch, (err, result) => {
     res.send(result);
     console.log("MySQL result successful from FRONT");
   });
 });
 
-// Serve up static assets (usually on heroku)
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-//   app.get("*", function (req, res) {
-//     const index = path.join(__dirname, "client/build", "index.html");
-//     res.sendFile(index);
-//   });
-// } else {
-// // simple route
+// // Route
 app.get("/*", (req, res) => {
   console.log("Successfully connection to LOCAL");
   res.sendFile(path.join(__dirname, "app/views/index.html"));
 });
-// }
 
 require("./app/routes/review.routes")(app);
 
-// set port, listen for requests
+// Set PORT. Listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(
